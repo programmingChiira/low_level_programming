e3b98ad73295bc546f7b8e295c890506d7be4262
Dennis Chiira <programmingchiira@gmail.com>
2023-03-30 14:13:21 -0700

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line.

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer to store result
+ * @size_r: buffer size
+ * Return: pointer to the result
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
